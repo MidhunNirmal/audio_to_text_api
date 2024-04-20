4601a6c45b4545600e32cba2bdfabef351c25f11
Midhun Nirmal <90173633+MidhunNirmal@users.noreply.github.com>
2024-04-20 18:58:22 +0530

    Update main.py ffffffffffffffffffffffff

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from typing import Union
-import requests
-from fastapi import FastAPI
+
+from fastapi import FastAPI,File, UploadFile
+
 import speech_recognition as sr
-from io import BytesIO
+
 
 app = FastAPI()
 
+
 @app.post("/audio")
-def read_root(audio_uri: str):
-    try:
-        # Download the audio file from the URI
-        response = requests.get(audio_uri)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-        
-        # Load audio data from the response content
-        audio_data = BytesIO(response.content)
-        
-        # Perform speech recognition
-        r = sr.Recognizer()
-        with sr.AudioFile(audio_data) as source:
-            audio = r.record(source)  # Extract audio data
-            
-        transcription = r.recognize_google(audio)
-        # Use Google Speech Recognition
-        print("audio is",transcription)
-        return {"transcription": transcription}
+def read_root(audio_file: UploadFile = File(...)):
     
-    except Exception as e:
-        return {"error": str(e)}
+    
+    r = sr.Recognizer()
+    with sr.WavFile(audio_file.file) as source:              # use "test.wav" as the audio source
+        audio = r.record(source)                        # extract audio data from the file
+
+    try:
+        print("Transcription: " + r.recognize(audio)) 
+        out = r.recognize(audio)
+        
+        # recognize speech using Google Speech Recognition
+    except LookupError:                                 # speech is unintelligible
+        print("Could not understand audio")
+    return {"Hello": out}
+
+
+
+
